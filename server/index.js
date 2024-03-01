412fbf1ad48948699114e73d57969618ff3fe36a
Dishankchovatia <dishankpatel03@gmail.com>
2024-03-01 11:08:21 +0530

    add login functionality

--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const userRoutes = require("/routes/userRoutes");
-const app = express();
+const authRoutes = require("./routes/auth");const app = express();
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", userRoutes);
+app.use("/api/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL, {
