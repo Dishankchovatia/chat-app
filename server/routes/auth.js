459eb5bc3ac008aa783fd9379d7a17a4bb17e4f2
Dishankchovatia <dishankpatel03@gmail.com>
2024-03-01 15:15:16 +0530

    chat container setup

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const {
     login,
     register,
     setAvatar,
+    getAllUsers
     
   } = require("../controllers/userController");
 
@@ -10,5 +11,6 @@ const router = require("express").Router();
 router.post("/register",register);
 router.post("/login",login);
 router.post("/setavatar/:id", setAvatar);
+router.get("/allusers/:id", getAllUsers);
 
 module.exports = router;
