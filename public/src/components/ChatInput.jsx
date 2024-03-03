54d5d1fb45b966fa2c3b48c8d682612dd70f882a
Dishankchovatia <dishankpatel03@gmail.com>
2024-03-03 15:57:59 +0530

    change in emoji selection

--- public/src/components/ChatInput.jsx
+++ public/src/components/ChatInput.jsx
@@ -70,29 +70,28 @@ const Container = styled.div`
         color: #ffff00c8;
         cursor: pointer;
       }
-      .emoji-picker-react {
+      .EmojiPickerReact {
         position: absolute;
-        top: -350px;
+        top: -500px;
         background-color: #080420;
         box-shadow: 0 5px 10px #9a86f3;
         border-color: #9a86f3;
-        .emoji-scroll-wrapper::-webkit-scrollbar {
+        .epr-body::-webkit-scrollbar {
           background-color: #080420;
           width: 5px;
           &-thumb {
             background-color: #9a86f3;
           }
         }
-        .emoji-categories {
-          button {
-            filter: contrast(0);
-          }
+        .epr-emoji-category-label {
+          background-color: #080420;
         }
-        .emoji-search {
+        
+        input {
           background-color: transparent;
           border-color: #9a86f3;
         }
-        .emoji-group:before {
+        .epr-emoji-group:before {
           background-color: #080420;
         }
       }
